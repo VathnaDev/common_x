fddb514660f61c51a9b9e00ec009790b5c035661
Vuthy Vathna <vathnadev@gmail.com>
2023-03-17 15:40:02 +0700

    rollback

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import '../example/lib/example_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    debugPrint(Platform.environment.toString());
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.dark,
+      showSemanticsDebugger: false,
+      debugShowCheckedModeBanner: false,
+      theme: _buildTheme(Brightness.dark),
+      home: ExampleScreen(),
+    );
+  }
+
+  ThemeData _buildTheme(brightness) {
+    var baseTheme = ThemeData(
+      primarySwatch: Colors.blue,
+      brightness: brightness,
+      useMaterial3: true,
+    );
+
+    return baseTheme.copyWith(
+      textTheme: GoogleFonts.montserratTextTheme(
+        baseTheme.textTheme,
+      ),
+    );
+  }
+}
